04bce3848f936d45ac390475c9f9bd2f0aa45b60
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-03-12 17:10:13 +0200

    Added navigation to login on heart click

--- adoption-website/src/components/Heart.jsx
+++ adoption-website/src/components/Heart.jsx
@@ -1,16 +1,23 @@
 import Checkbox from "@mui/material/Checkbox";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import Favorite from "@mui/icons-material/Favorite";
+import { useNavigate } from "react-router";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function IconCheckboxes() {
+  const navigateTo = useNavigate();
+
+  function handleOnClick() {
+    navigateTo("/logIn");
+  }
   return (
     <div>
       <Checkbox
         {...label}
         icon={<FavoriteBorder />}
         checkedIcon={<Favorite />}
+        onClick={() => handleOnClick()}
       />
     </div>
   );
