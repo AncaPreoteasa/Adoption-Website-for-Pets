d13aac90dc19c4daa0a0b3c2388c37d18bbb1ff7
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-03-20 16:48:02 +0200

    Updated loader with material ui

--- adoption-website/src/components/Loader.jsx
+++ adoption-website/src/components/Loader.jsx
@@ -1,3 +1,12 @@
-export default function () {
-  return <div>Loading...</div>;
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import styles from "./Loader.module.css";
+
+export default function Loader() {
+  return (
+    <Box sx={{ display: "flex" }} className={styles.loader}>
+      <CircularProgress />
+    </Box>
+  );
 }
