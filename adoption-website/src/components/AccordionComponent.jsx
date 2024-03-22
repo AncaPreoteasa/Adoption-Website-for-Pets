ad6551b8603a29f656b820d9825495be8c3c862c
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-03-22 13:34:39 +0200

    Added accordion component

---
+++ adoption-website/src/components/AccordionComponent.jsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import styles from "./AccordionComponent.module.css";
+
+export default function AccordionComponent() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={styles.accordionContainer}>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            Paperwork
+          </Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+            What paperwork do I need for adoption?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            Types of pets
+          </Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+            Am I a dog person or a cat person? Or perhaps a rabbit person?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat
+            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
+            laoreet laoreet.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>Costs</Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+            What are the costs for adoption?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            Information
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- adoption-website/src/components/Adoption.jsx
+++ adoption-website/src/components/Adoption.jsx
@@ -1,12 +1,17 @@
 import Header from "./Header";
 import NavBar from "./NavBar";
+import AccordionComponent from "./AccordionComponent";
+import styles from "./Adoption.module.css";
 
 export default function Adoption() {
   return (
     <>
       <Header />
       <NavBar />
-      <h1>HOW TO ADOPT</h1>
+      <div className={styles.adoptionContainer}>
+        <h3>Frequently asked questions</h3>
+        <AccordionComponent />
+      </div>
     </>
   );
 }
