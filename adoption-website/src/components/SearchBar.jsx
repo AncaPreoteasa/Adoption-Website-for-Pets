893edfa12a6b92caf456daa21f343f008f0b81ff
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-04-03 18:12:35 +0300

    On submit navigate to another component

--- adoption-website/src/components/SearchBar.jsx
+++ adoption-website/src/components/SearchBar.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
 import searchImage from "../assets/search.png";
+import { useNavigate } from "react-router-dom";
 
 export function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate();
+
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    navigate("/pets");
   };
 
   return (
