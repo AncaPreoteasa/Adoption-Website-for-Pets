0244e85398805056240e93f04b3257f1842eb0f4
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-04-18 15:38:01 +0300

    Optimized bundle size with code splitting

--- adoption-website/src/App.jsx
+++ adoption-website/src/App.jsx
@@ -1,36 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Pets from "./components/Pets";
-import Layout from "./components/Layout";
-import Cats from "./components/Cats";
-import Dogs from "./components/Dogs";
-import Articles from "./components/Articles";
-import NoPage from "./components/NoPage";
-import Contact from "./components/Contact";
-import Adoption from "./components/Adoption";
-import PetDetails from "./components/PetDetails";
-import SignUp from "./components/SignUp";
-import LogIn from "./components/LogIn";
-import Other from "./components/Other";
 import "./App.css";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+
+const Layout = lazy(() => import("./components/Layout"));
+const Pets = lazy(() => import("./components/Pets"));
+const Cats = lazy(() => import("./components/Cats"));
+const Dogs = lazy(() => import("./components/Dogs"));
+const Articles = lazy(() => import("./components/Articles"));
+const NoPage = lazy(() => import("./components/NoPage"));
+const Contact = lazy(() => import("./components/Contact"));
+const Adoption = lazy(() => import("./components/Adoption"));
+const PetDetails = lazy(() => import("./components/PetDetails"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const LogIn = lazy(() => import("./components/LogIn"));
+const Other = lazy(() => import("./components/Other"));
+const SearchedPets = lazy(() => import("./components/SearchedPets"));
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}></Route>
-          <Route path="pets" element={<Pets />} />
-          <Route path="dogs" element={<Dogs />} />
-          <Route path="cats" element={<Cats />} />
-          <Route path="other" element={<Other />} />
-          <Route path="articles" element={<Articles />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="adoption" element={<Adoption />} />
-          <Route path="signUp" element={<SignUp />} />
-          <Route path="logIn" element={<LogIn />} />
-          <Route path="pet-details/:id" element={<PetDetails />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}></Route>
+            <Route path="pets" element={<Pets />} />
+            <Route path="searchedPets" element={<SearchedPets />} />
+            <Route path="dogs" element={<Dogs />} />
+            <Route path="cats" element={<Cats />} />
+            <Route path="other" element={<Other />} />
+            <Route path="articles" element={<Articles />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="adoption" element={<Adoption />} />
+            <Route path="signUp" element={<SignUp />} />
+            <Route path="logIn" element={<LogIn />} />
+            <Route path="pet-details/:id" element={<PetDetails />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
